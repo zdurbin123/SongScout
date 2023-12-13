f71aaf9a1bd71e0c4ecd58429b683c4fb4cbb070
Yuehan Qin <64568803+JJthomps-prog@users.noreply.github.com>
2023-12-13 11:25:20 -0500

    Update music.js

--- data/music.js
+++ data/music.js
@@ -11,6 +11,7 @@ import {
     deleteDoc,
     updateDoc,
     getDoc,
+    orderBy
   } from "firebase/firestore/lite";
   import { initializeApp } from "firebase/app";
   import FirebaseConfig from '../src/firebase/FirebaseConfig'
@@ -18,8 +19,10 @@ import {
 //uid is user_id
 const firebaseApp = initializeApp(FirebaseConfig);
 const db = getFirestore(firebaseApp);
-async function likeSong(uid, { song_name, song_id, artists, preview_url }) {
+const likesCollection = collection(db, 'likes');
+async function likeSong(uid, { song_name, song_id, artists, preview_url,song_url,image_url }) {
     const userDocRef = doc(db, 'users', uid);
+    const likesDocRef = doc(likesCollection, song_id);
   
     try {
       const userDoc = await getDoc(userDocRef);
@@ -31,12 +34,26 @@ async function likeSong(uid, { song_name, song_id, artists, preview_url }) {
           song_id,
           artists,
           preview_url,
+          song_url,
+          image_url
         };
-  
-        likedsongs.push(likedSong);
-  
-        await updateDoc(userDocRef, { likedsongs });
-        console.log('Liked song added successfully!');
+        const songExists = likedsongs.some(song => song.song_id === song_id);
+
+        if (!songExists) {
+          likedsongs.push(likedSong);
+          await updateDoc(userDocRef, { likedsongs });
+          //update likes collection
+          const likesDoc = await getDoc(likesDocRef);
+                if (likesDoc.exists()) {
+                    const currentLikes = likesDoc.data().likes || 0;
+                    await updateDoc(likesDocRef, { likes: currentLikes + 1 });
+                } else {
+                    await setDoc(likesDocRef, { likedSong,likes: 1 });
+                }
+          console.log('Liked song successfully!');
+        } else {
+          console.log('Song already liked!');
+        }
       } else {
         console.error('User not found');
       }
@@ -44,10 +61,12 @@ async function likeSong(uid, { song_name, song_id, artists, preview_url }) {
       console.error('Error adding liked song:', error);
       throw error;
     }
-  }
+}
+
 
   async function dislikeSong(uid, song_id) {
     const userDocRef = doc(db, 'users', uid);
+    const likesDocRef = doc(likesCollection, song_id);
   
     try {
       const userDoc = await getDoc(userDocRef);
@@ -56,6 +75,12 @@ async function likeSong(uid, { song_name, song_id, artists, preview_url }) {
         const likedsongs = userData.likedsongs || [];
         const updatedLikedsongs = likedsongs.filter((likedSong) => likedSong.song_id !== song_id);
         await updateDoc(userDocRef, { likedsongs: updatedLikedsongs });
+        //update likes collection
+        const likesDoc = await getDoc(likesDocRef);
+            if (likesDoc.exists()) {
+                const currentLikes = likesDoc.data().likes || 0;
+                await updateDoc(likesDocRef, { likes: Math.max(0, currentLikes - 1) });
+            }
         console.log('Liked song removed successfully!');
       } else {
         console.error('User not found');
@@ -91,10 +116,30 @@ async function likeSong(uid, { song_name, song_id, artists, preview_url }) {
     }
   }
 
+  async function getLikedSongsSortedByLikes() {
+    try {
+        const querySnapshot = await getDocs(query(
+            likesCollection,
+            where('likes', '>', 0),
+            orderBy('likes', 'desc')
+        ));
+
+        const likedSongs = [];
+        querySnapshot.forEach((doc) => {
+            likedSongs.push(doc.data());
+        });
+
+        return likedSongs;
+    } catch (error) {
+        console.error('Error getting liked songs sorted by likes:', error);
+        throw error;
+    }
+}
 
   
   export{
     likeSong,
     dislikeSong,
-    getLikedSongs
+    getLikedSongs,
+    getLikedSongsSortedByLikes
   }
