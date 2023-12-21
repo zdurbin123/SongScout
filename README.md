a2ccec2967f3c4e8370a58b2b83c3c651c3f4a3f
Jey Sneha Arul Joseph <jeysneha1@gmail.com>
2023-12-21 03:22:12 -0500

    final push yay

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 CS554 Final Project
 
 Our project allows users to have more customizable recommendation options than the regular Spotify website. Find songs using the audio features that aren't usually available on Spotify search options. 
-
-## Hosting the Website
-To host the website, the server must be run locally, using the command "npm start". ImageMagick is hosted locally to manipulate images, which are then stored on the cloud with FireStore.
+## Install ImageMagick and Redis locally on the computer
+Use the following command to install ImageMagick:(Mac/Windows)
+Use the following command to install redis:(Mac/Windows)
+## Start the Redis Server
+Run the Redis Server in the terminal locally using the commands redis-server, redis-cli
+## Must Run The Express Server Locally:
+Execute the command npm start in the terminal. This will start the express backend server for the SongScout application. ImageMagick is hosted locally to manipulate images, which are then stored on the cloud with FireStore.
+## AWS Amplify Hosting:
+The frontend of our application is hosted in the aws amplify.You can access the live application at the following URL: 
+## Running Locally:
+Alternatively, Our react application can also run locallly using Vite by using the commmand npm run dev. 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,12 +22,12 @@ function Home() {
           </Card.Text>
           <br/>
           <ListGroup variant="flush">
-            <ListGroup.Item>Trending section:</ListGroup.Item>
-            <ListGroup.Item>Recommendations Section:</ListGroup.Item>
-            <ListGroup.Item>Song Section:</ListGroup.Item>
-            <ListGroup.Item>Tracks/Artists Section:</ListGroup.Item>
-            <ListGroup.Item>Account Section:</ListGroup.Item>
-            <ListGroup.Item>MySongs Section:</ListGroup.Item>
+            <ListGroup.Item>Trending section: Displays The Top 10 most liked songs of our application</ListGroup.Item>
+            <ListGroup.Item>Recommendations Section: Finds customized recommendations that are not natively searchable on Spotify </ListGroup.Item>
+            <ListGroup.Item>Song Section: Shows the Song Info Slide with audio preview that fades into the Song's Artist Info</ListGroup.Item>
+            <ListGroup.Item>Tracks/Artists Section: search for tracks and artists from most to least popular</ListGroup.Item>
+            <ListGroup.Item>Account Section: Displays user profile with liked and disliked songs</ListGroup.Item>
+            <ListGroup.Item>MySongs Section:Finds recommendations based of what the user liked</ListGroup.Item>
           </ListGroup> 
           <Card.Text style={{ fontWeight: 'bold' ,color: '#0D98BA'}} >
             Our SongScout team is excited to have you on this musical journey with us. Start exploring now and find your next favorite song with SongScout!
