6f45fc189eca3eb0e001ef65922d6f634fed04b8
Jey Sneha Arul Joseph <jeysneha1@gmail.com>
2023-12-21 02:24:50 -0500

    recomm err done

--- data/validation.js
+++ data/validation.js
@@ -1,4 +1,4 @@
-let alphaCodes = ["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BH", 
+let alphaCodes = ["AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BH", 
 "BS", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", 
 "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CG", 
 "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ",
@@ -15,7 +15,22 @@ let alphaCodes = ["AF", "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "A
   "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"]
 
 function checkMarket (market) {
-    if(market&&!(alphaCodes.includes(market))) return "Market must be an ISO 3166-1 alpha-2 country code.";
+    if (market&&market.trim() === "") return "Market cant be empty string/just spaces";
+    if(market&&!(alphaCodes.includes(market.trim().toUpperCase()))) {return `Market must be an ISO 3166-1 alpha-2 country code. Correct codes are: "AX", "AL", "DZ", "AS", "AD", "AO", "AI", "AQ", "AG", "AR", "AM", "AW", "AU", "AT", "AZ", "BH", 
+    "BS", "BD", "BB", "BY", "BE", "BZ", "BJ", "BM", "BT", "BO", "BQ", "BA", "BW", "BV", "BR", "IO", "BN", 
+    "BG", "BF", "BI", "KH", "CM", "CA", "CV", "KY", "CF", "TD", "CL", "CN", "CX", "CC", "CO", "KM", "CG", 
+    "CD", "CK", "CR", "CI", "HR", "CU", "CW", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ",
+     "ER", "EE", "ET", "FK", "FO", "FJ", "FI", "FR", "GF", "PF", "TF", "GA", "GM", "GE", "DE", "GH", "GI", 
+     "GR", "GL", "GD", "GP", "GU", "GT", "GG", "GN", "GW", "GY", "HT", "HM", "VA", "HN", "HK", "HU", "IS", 
+     "IN", "ID", "IR", "IQ", "IE", "IM", "IL", "IT", "JM", "JP", "JE", "JO", "KZ", "KE", "KI", "KP", "KR", 
+     "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MO", "MK", "MG", "MW", "MY", "MV", 
+     "ML", "MT", "MH", "MQ", "MR", "MU", "YT", "MX", "FM", "MD", "MC", "MN", "ME", "MS", "MA", "MZ", "MM", 
+     "NA", "NR", "NP", "NL", "NC", "NZ", "NI", "NE", "NG", "NU", "NF", "MP", "NO", "OM", "PK", "PW", "PS", 
+     "PA", "PG", "PY", "PE", "PH", "PN", "PL", "PT", "PR", "QA", "RE", "RO", "RU", "RW", "BL", "SH", "KN",
+      "LC", "MF", "PM", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SX", "SK", "SI", "SB", 
+      "SO", "ZA", "GS", "SS", "ES", "LK", "SD", "SR", "SJ", "SZ", "SE", "CH", "SY", "TW", "TJ", "TZ", "TH", 
+      "TL", "TG", "TK", "TO", "TT", "TN", "TR", "TM", "TC", "TV", "UG", "UA", "AE", "GB", "US", "UM", "UY", 
+      "UZ", "VU", "VE", "VN", "VG", "VI", "WF", "EH", "YE", "ZM", "ZW"`}
     else return false;
 }
 
@@ -33,13 +48,14 @@ let availableGenres = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambie
     "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]
 
 function checkGenres (genre) {
-    if (!genre) return "Genre must be provided";
+    if (!genre || genre.trim() === "") return "Genre must be provided";
     let genres = genre.split(",");
-    console.log(genres);
+    if(genres.length>5) return "Up to 5 Genre seed values may be provided tops!"
     for(let g of genres){
-        let newG = g.toLowerCase();
+        let newG = g.trim().toLowerCase();
+        if (!newG) return "Genre list contains empty genre";
         console.log(newG);
-        if(!(availableGenres.includes(newG))) return "Genre must be on of the available genres on Spotify.";
+        if(!(availableGenres.includes(newG))) {return `Genre must be on of the available genres on Spotify!!!   Available Genres: "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial","iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks","spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"`;}
     }
     return false;
     
@@ -53,6 +69,7 @@ function validateSpotifyArtistID(id) {
 function checkArtists(artistList) {
     if (artistList) {
     const artists = artistList.split(",");
+    if(artists.length>5) return "Up to 5 Artists seed values may be provided tops!"
     for (let artistID of artists) {
         if (!validateSpotifyArtistID(artistID)) {
             return "Invalid Spotify artist ID/IDs detected.";
@@ -70,6 +87,7 @@ function validateSpotifyTrackID(id) {
 function checkTracks(trackList) {
     if (trackList) {
     const tracks = trackList.split(",");
+    if(tracks.length>5) return "Up to 5 tracks seed values may be provided tops!"
     for (let trackID of tracks) {
         if (!validateSpotifyTrackID(trackID)) {
             return "Invalid Spotify track ID/IDs detected.";
@@ -96,6 +114,13 @@ function checkAcousticness (value){
 function checkDancability (value){
     if(value&&(Number.isNaN(value) || value == "")) return "Dancability must be a number";
     if(value < 0 || value > 1) return " Dancability must be between 0 and 1";
+    else{
+        return false;
+    }
+}
+function checkValence (value){
+    if(value&&(Number.isNaN(value) || value == "")) return "Valence must be a number";
+    if(value < 0 || value > 1) return " Valence must be between 0 and 1";
     else{
         return false;
     }
@@ -167,7 +192,8 @@ export {
     checkPopCross,
     checkTempoCross,
     checkArtists,
-    checkTracks
+    checkTracks,
+    checkValence
    
 
 
